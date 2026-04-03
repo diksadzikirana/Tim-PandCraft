354dd166e5ae911e1fb450cdc388c3bb6bd4643c
VeraAndini <veraandini546@gmail.com>
2026-04-03 22:31:32 +0700

    revisi session

---
+++ session.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+function createSession($data) {
+    $_SESSION['user'] = $data['user'];
+    $_SESSION['exp'] = time() + 10; // 10 detik
+}
+
+function getValidSession() {
+    if (!isset($_SESSION['user']) || !isset($_SESSION['exp'])) {
+        return false;
+    }
+
+    if (time() > $_SESSION['exp']) {
+        session_unset();
+        session_destroy();
+        return false;
+    }
+
+    return ["user" => $_SESSION['user']];
+}
+?>
